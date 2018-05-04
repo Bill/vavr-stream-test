16b76371f3d4926577c79f86899bb5dbc7dd7801
Bill <bill@brainspace.com>
2018-05-04 12:02:18 -0700

    tweak to drop(SIZE) instead of SIZE/2

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -8,16 +8,18 @@ public class StreamTest {
 
     @Test
     public void first() {
-        final int SIZE = (int)10E8;
-        assertEquals(SIZE, Stream
+        final int SIZE = (int)10E7;
+        assertEquals(2 * (SIZE +1), Stream
                 .iterate(1, i->i+1)
                 .filter(StreamTest::isEven)
                 .map(Object::toString)
                 .map(Integer::parseInt)
-                .drop(SIZE/2 - 1).head().intValue());
+                .drop(SIZE)
+                .head()
+                .intValue());
     }
 
-    private static boolean isEven(final Integer i) {
+    private static boolean isEven(final int i) {
         return i % 2 == 0;
     }
 }
