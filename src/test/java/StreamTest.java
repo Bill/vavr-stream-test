4a876c47e0a838383a827a117c128cd5b468df7b
Bill <bill@brainspace.com>
2018-05-04 11:16:57 -0700

    don't hold on to head

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -8,8 +8,7 @@ public class StreamTest {
 
     @Test
     public void first() {
-        final Stream<Integer> s = Stream.iterate(1, i->i+1);
-        final int SIZE = (int)10E7;
-        assertEquals(SIZE, s.drop(SIZE-1).head().intValue());
+        final int SIZE = (int)10E8;
+        assertEquals(SIZE, Stream.iterate(1, i->i+1).drop(SIZE - 1).head().intValue());
     }
 }
