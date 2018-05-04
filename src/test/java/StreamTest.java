bb231a657c7780d934448dbf85a354b23ad10a89
Bill <bill@brainspace.com>
2018-05-04 16:34:46 -0700

    fall back to JUnit 4; clarify test

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -1,22 +1,46 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Ignore;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
-
+import io.vavr.collection.LinearSeq;
 import io.vavr.collection.Stream;
+import io.vavr.collection.Traversable;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class StreamTest {
 
+    private static final int SIZE = (int)1E1;
+
     @Test
-    public void first() {
-        final int SIZE = (int)10E7;
-        assertEquals(2 * (SIZE +1), Stream
-                .iterate(1, i->i+1)
-                .filter(StreamTest::isEven)
+    public void simplestTest() {
+        assertThat(SIZE+1, is(process(naturals(), SIZE)));
+    }
+
+    private static int process(final Traversable<Integer> traversable, final int size) {
+        return traversable.drop(size).head();
+    }
+
+    private static Stream<Integer> naturals() {
+        return Stream
+                .iterate(1, i->i+1);
+    }
+
+    @Ignore
+    @Test
+    public void arbitrarilyComplexTest() {
+        assertThat(2 * (SIZE +1), is(process(roundTripToStringAndBack(evenNaturals()), SIZE)));
+    }
+
+    private static Stream<Integer> evenNaturals() {
+        return naturals()
+                .filter(StreamTest::isEven);
+    }
+
+    private static LinearSeq<Integer> roundTripToStringAndBack(final LinearSeq<Integer> integers) {
+        return integers
                 .map(Object::toString)
-                .map(Integer::parseInt)
-                .drop(SIZE)
-                .head()
-                .intValue());
+                .map(Integer::parseInt);
     }
 
     private static boolean isEven(final int i) {
