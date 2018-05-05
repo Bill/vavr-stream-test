113d1aa9a81609a89f379c45b6ef75da61e7453f
Bill <bill@brainspace.com>
2018-05-04 21:16:16 -0700

    added vavr Iterator tests

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -1,6 +1,7 @@
 import org.junit.Ignore;
 import org.junit.Test;
 
+import io.vavr.collection.Iterator;
 import io.vavr.collection.LinearSeq;
 import io.vavr.collection.Stream;
 import io.vavr.collection.Traversable;
@@ -10,31 +11,72 @@ import static org.junit.Assert.assertThat;
 
 public class StreamTest {
 
-    private static final int SIZE = (int)1E1;
+    private static final int SIZE = (int)1E8;
 
     @Test
-    public void simplestTest() {
-        assertThat(SIZE+1, is(process(naturals(), SIZE)));
+    public void simplestIteratorScalingTest() {
+        assertThat(process(naturalsIterator(), SIZE), is(SIZE+1));
     }
 
-    private static int process(final Traversable<Integer> traversable, final int size) {
-        return traversable.drop(size).head();
+    @Test
+    public void arbitrarilyComplexIteratorScalingTest() {
+        assertThat(process(roundTripToStringAndBack(evenNaturalsIterator()), SIZE), is(2 * (SIZE +1)));
     }
 
-    private static Stream<Integer> naturals() {
+    @Ignore // because vavr Iterator is NOT immutable
+    @Test
+    public void iteratorImmutabilityTest() {
+        final Iterator<Integer> sixPlus = naturalsIterator().drop(5);
+        assertThat(sixPlus.drop(5).head(), is(11));
+        assertThat(sixPlus.drop(5).head(), is(11)); // FAILS!!!!
+    }
+
+    @Ignore // because vavr Stream does not discard head, so memory consumption increases as you iterate!
+    @Test
+    public void simplestStreamScalingTest() {
+        assertThat(process(naturalsStream(), SIZE), is(SIZE+1));
+    }
+
+    @Ignore // (inlining, unsurprisingly, doesn't help)
+    @Test
+    public void simplestStreamScalingTestInlined() {
+        assertThat(Stream.iterate(1, i -> i + 1).drop(SIZE).head(), is(SIZE+1));
+    }
+
+    @Ignore // because vavr Stream does not discard head, so memory consumption increases as you iterate!
+    @Test
+    public void arbitrarilyComplexStreamScalingTest() {
+        assertThat(process(roundTripToStringAndBack(evenNaturalsStream()), SIZE), is(2 * (SIZE +1)));
+    }
+
+    private static Stream<Integer> naturalsStream() {
         return Stream
                 .iterate(1, i->i+1);
     }
 
-    @Ignore
-    @Test
-    public void arbitrarilyComplexTest() {
-        assertThat(2 * (SIZE +1), is(process(roundTripToStringAndBack(evenNaturals()), SIZE)));
+    private static Iterator<Integer> naturalsIterator() {
+        return Iterator.iterate(1, i-> i + 1);
     }
 
-    private static Stream<Integer> evenNaturals() {
-        return naturals()
+    private static Iterator<Integer> evenNaturalsIterator() {
+        return naturalsIterator()
                 .filter(StreamTest::isEven);
+    }
+
+    private static Stream<Integer> evenNaturalsStream() {
+        return naturalsStream()
+                .filter(StreamTest::isEven);
+    }
+
+    private static boolean isEven(final int i) {
+        return i % 2 == 0;
+    }
+
+
+    private static Iterator<Integer> roundTripToStringAndBack(final Iterator<Integer> integers) {
+        return integers
+                .map(Object::toString)
+                .map(Integer::parseInt);
     }
 
     private static LinearSeq<Integer> roundTripToStringAndBack(final LinearSeq<Integer> integers) {
@@ -43,7 +85,9 @@ public class StreamTest {
                 .map(Integer::parseInt);
     }
 
-    private static boolean isEven(final int i) {
-        return i % 2 == 0;
+
+    private static int process(final Traversable<Integer> traversable, final int size) {
+        return traversable.drop(size).head();
     }
+
 }
